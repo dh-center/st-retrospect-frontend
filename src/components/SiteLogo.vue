b6b35e296fe653f8d2c1ab1076cd4f276e7c4f94
ilyamore88 <themorozik@gmail.com>
2020-01-02 02:45:42 +0300

    add SiteLogo.vue component

---
+++ src/components/SiteLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <a href="/" class="site-logo">St.Retrospect</a>
+</template>
+
+<script>
+export default {
+  name: 'SiteLogo'
+};
+</script>
+
+<style>
+  .site-logo {
+    font-family: 'Oranienbaum', serif;
+    font-size: 28px;
+    color: #ffffff;
+    text-decoration: none;
+  }
+</style>
