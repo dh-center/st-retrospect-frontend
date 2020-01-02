0e3890ad8419cb6ac8a91e808fafb35f67f9a1ab
Nikita Melnikov <nikmel2803@gmail.com>
2020-01-02 18:45:25 +0300

    Create view with map (#4)
    * setup mapbox
    
    * remove token
    
    * add .env file creating in CI
    
    * fix workflow
    
    * debug workflow
    
    * test
    
    * test
    
    * meow
    
    * debug
    
    * remove log
    
    * add named webpack chunk
    
    * remove unnessesary workflow code
    
    * remove unnecessary code from Dockerfile

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,10 +19,16 @@ const routes = [
      * which is lazy-loaded when the route is visited.
      */
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/map',
+    name: 'map',
+    component: () => import(/* webpackChunkName: "mapView" */ '../views/Map.vue')
   }
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 });
 
