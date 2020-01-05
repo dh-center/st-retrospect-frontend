8f17c100ab77b38c0c0099778a1e6aefe811d4aa
ilyamore88 <themorozik@gmail.com>
2020-01-06 00:17:25 +0300

    init vuex

---
+++ src/store/app/actionTypes.ts
@@ -0,0 +1,9 @@
+/**
+ * Set another language for displaying data from database
+ */
+export const CHANGE_DATA_LANG = 'CHANGE_DATA_LANG';
+
+/**
+ * Set another language for user interface
+ */
+export const CHANGE_INTERFACE_LANG = 'CHANGE_INTERFACE_LANG';
