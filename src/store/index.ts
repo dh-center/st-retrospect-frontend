76e9bbcd8bed4584eb3bd98339a11c82866602da
ilyamore88 <themorozik@gmail.com>
2020-01-06 00:15:15 +0300

    init persistedstate

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import createPersistedState from 'vuex-persistedstate';
+
 Vue.use(Vuex);
 
+const debug = process.env.NODE_ENV !== 'production';
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  plugins: [ createPersistedState() ],
+  strict: debug
 });
