567451be4615503feaa6cdb984e0a43d371120fa
ilyamore88 <themorozik@gmail.com>
2020-01-07 02:37:06 +0300

    create simple markers and add it to map

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <div id="mapContainer" />
+    <div id="mapContainer">
+      <MapMarker
+        v-if="map"
+        :map="map"
+        :lng-lat="[30.28617, 59.93944]"
+      />
+      <MapMarker
+        v-if="map"
+        :map="map"
+        :lng-lat="[30.29617, 59.93944]"
+      />
+      <MapMarker
+        v-if="map"
+        :map="map"
+        :lng-lat="[30.27617, 59.93944]"
+      />
+    </div>
     <MapAside />
   </div>
 </template>
@@ -9,12 +25,14 @@
 import { Component, Vue } from 'vue-property-decorator';
 import mapboxgl from 'mapbox-gl';
 import MapAside from '@/components/MapAside.vue';
+import MapMarker from '@/components/MapMarker.vue';
 
 mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN as string;
 
 @Component({
   components: {
-    MapAside
+    MapAside,
+    MapMarker
   }
 })
 /**
@@ -24,7 +42,7 @@ export default class MapView extends Vue {
   /**
    * MapboxGL map instance
    */
-  private map?: mapboxgl.Map;
+  private map?: mapboxgl.Map | null = null;
 
   /**
    * Vue mounted hook
