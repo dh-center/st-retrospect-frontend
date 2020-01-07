f361544b68899925a7a4843c5ee6a73a61fb8139
ilyamore88 <themorozik@gmail.com>
2020-01-07 19:38:07 +0300

    add HTMLElement to mapbox marker, add prop for marker svg name

--- src/components/MapMarker.vue
+++ src/components/MapMarker.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="marker-block" />
+  <div
+    :id="markerElementId"
+    class="marker-block"
+  >
+    <svg
+      v-svg
+      :symbol="markerSvgName"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import mapboxgl from 'mapbox-gl';
 
-@Component
-/**
- * Component of marker on map
- */
+  @Component
+  /**
+   * Component of marker on map
+   */
 export default class MapMarker extends Vue {
   /**
    * MapboxGL marker instance
    */
   private marker?: mapboxgl.Marker;
+
+  /**
+   * Unique ID of marker pint HTML element
+   */
+  private markerElementId: string = this.$id('marker-point');
 
   /**
    * MapboxGL map for adding marker
@@ -29,13 +42,36 @@ export default class MapMarker extends Vue {
   private lngLat!: [number, number];
 
   /**
+   * Point svg name
+   */
+  @Prop({ type: String, required: true })
+  private markerSvgName!: string;
+
+  /**
    * Vue mounted hook
    * Setups marker for displaying
    */
   mounted() {
-    this.marker = new mapboxgl.Marker()
+    this.marker = new mapboxgl.Marker({
+      element: document.getElementById(this.markerElementId) as HTMLElement
+    })
       .setLngLat(this.lngLat)
       .addTo(this.map);
   }
 }
 </script>
+
+<style>
+  .marker-block {
+    width: 30px;
+    height: 40px;
+
+    svg {
+      position: relative;
+      top: -20px;
+
+      width: 30px;
+      height: 40px;
+    }
+  }
+</style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -5,16 +5,19 @@
         v-if="map"
         :map="map"
         :lng-lat="[30.28617, 59.93944]"
+        marker-svg-name="actor-home"
       />
       <MapMarker
         v-if="map"
         :map="map"
         :lng-lat="[30.29617, 59.93944]"
+        marker-svg-name="theater"
       />
       <MapMarker
         v-if="map"
         :map="map"
         :lng-lat="[30.27617, 59.93944]"
+        marker-svg-name="study"
       />
     </div>
     <MapAside />
