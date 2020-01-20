144f3f01766efd9b5fe7871d48766b1cf32ce522
Nikita Melnikov <nikmel2803@gmail.com>
2020-01-21 00:00:56 +0300

    debug

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,6 +2,10 @@
 
 # Decrypt the file
 mkdir $HOME/secrets
+
+pwd
+echo pwd
+
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
 --output $HOME/secrets/.env ../.env.gpg
