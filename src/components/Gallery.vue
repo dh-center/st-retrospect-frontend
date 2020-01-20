dd48338d84bc8ce3a2a50280f80a3f32e502501d
ilyamore88 <themorozik@gmail.com>
2020-01-20 22:31:00 +0300

    style Gallery.vue, add lightgallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -3,17 +3,57 @@
     <div class="photos__title">
       {{ $t('photos') }}
     </div>
+    <LightGallery
+      :images="images"
+      :index="currentImageIndex"
+      @close="currentImageIndex = null"
+    />
+    <div
+      id="photo-gallery"
+      class="photos__list"
+    >
+      <div
+        v-for="(thumb, thumbIndex) in images"
+        :key="thumbIndex"
+        class="photos__item"
+        :style="{'background-image': `url(${thumb})`}"
+        @click="currentImageIndex = thumbIndex"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { LightGallery } from 'vue-light-gallery';
 
-@Component
+@Component({
+  components: {
+    LightGallery
+  }
+})
 /**
  * Component for photos block in aside bar
  */
 export default class Gallery extends Vue {
+  images = [
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg'),
+    require('../assets/images/person-125-main.jpeg')
+  ];
+
+  currentImageIndex: number | null = null;
 }
 </script>
 
@@ -41,6 +81,21 @@ export default class Gallery extends Vue {
 
       color: #2d2d2d;
       font-size: 20px;
+    }
+
+    &__list {
+      display: grid;
+      grid-row-gap: 15px;
+      grid-template-columns: 115px 115px 115px;
+      justify-content: space-between;
+    }
+
+    &__item {
+      width: 115px;
+      height: 100px;
+
+      background-position: center;
+      background-size: cover;
     }
   }
 </style>
