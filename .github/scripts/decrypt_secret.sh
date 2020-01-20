154754f5b702ac36fa0d9eba79ca9fa2e5b3fe1b
Nikita Melnikov <nikmel2803@gmail.com>
2020-01-20 23:56:37 +0300

    fix file path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output $HOME/secrets/.env .env.gpg
+--output $HOME/secrets/.env ../.env.gpg
