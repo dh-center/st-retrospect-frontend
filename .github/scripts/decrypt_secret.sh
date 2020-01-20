71fc6d17843f8a107531d2795b1574702368b055
Nikita Melnikov <nikmel2803@gmail.com>
2020-01-21 00:13:30 +0300

    remove debug commands

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
-
-pwd
-ls
-
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output $HOME/secrets/.env .github/.env.gpg
+--output .env .github/.env.gpg
