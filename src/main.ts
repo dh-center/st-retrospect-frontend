0449849f7c175fcef658e10e1c521f6af3bc256b
Nikita Melnikov <nikmel2803@gmail.com>
2020-01-20 23:46:42 +0300

    setup sentry and secrets decoding

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,18 @@ import UniqueId from 'vue-unique-id';
 import SvgSprite from 'vue-svg-sprite';
 import i18n from './i18n';
 import VModal from 'vue-js-modal';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 
 Vue.use(VModal, { dynamic: true, injectModalsContainer: true });
 Vue.use(UniqueId);
 Vue.use(SvgSprite, {
   url: require('./assets/sprite.svg')
+});
+
+Sentry.init({
+  dsn: 'https://20d5225f0be642899c1a75c49114305e@sentry.io/1888402',
+  integrations: [ new Integrations.Vue({ Vue, attachProps: true }) ]
 });
 
 Vue.config.productionTip = false;
@@ -21,3 +28,5 @@ new Vue({
   i18n,
   render: h => h(App)
 }).$mount('#app');
+
+throw new Error('kek');
