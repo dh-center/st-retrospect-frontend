0c68653579456a10e562e1d682198f1608143f56
Nikita Melnikov <nikmel2803@gmail.com>
2020-01-21 00:40:42 +0300

    setup sentry and secrets decoding (#32)
    * setup sentry and secrets decoding
    
    * workflow fix
    
    * debug
    
    * fix folder name
    
    * fix file path
    
    * debug
    
    * debug
    
    * change folder name
    
    * remove debug commands
    
    * get sentry dsn from .env
    
    * remove sample error

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,18 @@ import UniqueId from 'vue-unique-id';
 import SvgSprite from 'vue-svg-sprite';
 import i18n from './i18n';
 import VModal from 'vue-js-modal';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 
 Vue.use(VModal, { dynamic: true, injectModalsContainer: true });
 Vue.use(UniqueId);
 Vue.use(SvgSprite, {
   url: require('./assets/sprite.svg')
+});
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [ new Integrations.Vue({ Vue, attachProps: true }) ]
 });
 
 Vue.config.productionTip = false;
