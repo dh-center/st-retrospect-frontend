4d6cac8c7014462fe7c8b70aad85edee9604c57c
ilyamore88 <themorozik@gmail.com>
2021-02-04 18:42:19 +0300

    configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
 
-function App() {
+/**
+ * Main component of the application
+ */
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
