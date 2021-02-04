f70cabeda33ff0cb08aea7f98d66e509f3a6baa1
ilyamore88 <themorozik@gmail.com>
2021-02-04 21:39:11 +0300

    implements react-router-dom

---
+++ src/components/Map.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react';
+
+/**
+ * Component displays map
+ */
+function Map(): ReactElement {
+  return (
+    <h1>Map component</h1>
+  );
+}
+
+export default Map;
---
+++ src/views/HomeView.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+import Map from '../components/Map';
+
+/**
+ * View of home page
+ */
+function HomeView(): ReactElement {
+  return (
+    <div>
+      <Map/>
+    </div>
+  );
+}
+
+export default HomeView;
