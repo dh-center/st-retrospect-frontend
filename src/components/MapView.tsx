b32ed62a33964d4dc4cd265c3493e59be2e165c0
ilyamore88 <themorozik@gmail.com>
2021-02-04 22:30:20 +0300

    implement MapView, add mapbox access token

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,11 +1,80 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useRef, useState } from 'react';
+import mapboxgl, { LngLatBoundsLike } from 'mapbox-gl';
+import styled from 'styled-components';
+
+const MapContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
+
+/**
+ * Max map bounds
+ * Restricts view area
+ */
+const MAX_BOUNDS: LngLatBoundsLike = [
+  /**
+   * Southwest coordinates
+   */
+  [
+    29.281524984914313,
+    59.62023377214044,
+  ],
+
+  /**
+   * Northeast coordinates
+   */
+  [
+    31.263740364566985,
+    60.282501691026226,
+  ],
+];
 
 /**
  * Component displays map view
  */
 function MapView(): ReactElement {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<mapboxgl.Map>();
+  const [mapState, setMapState] = useState({
+    lng: 30.28617,
+    lat: 59.93944,
+    zoom: 11.5,
+  });
+
+  useEffect(() => {
+    if (mapContainer.current) {
+      map.current = new mapboxgl.Map({
+        attributionControl: false,
+        logoPosition: 'bottom-right',
+        container: mapContainer.current,
+        style: 'mapbox://styles/dandriver/ck0epf0pe0qh51cr3ecw3v65y',
+        center: [mapState.lng, mapState.lat],
+        zoom: mapState.zoom,
+        maxBounds: MAX_BOUNDS,
+      });
+
+      const navigationControl = new mapboxgl.NavigationControl();
+      const fullscreenControl = new mapboxgl.FullscreenControl();
+      const attributionControl = new mapboxgl.AttributionControl({ compact: true });
+
+      map.current.addControl(fullscreenControl, 'top-right');
+      map.current.addControl(navigationControl, 'top-right');
+      map.current.addControl(attributionControl, 'bottom-right');
+
+      map.current.on('move', () => {
+        map.current && setMapState({
+          lng: +map.current.getCenter().lng.toFixed(4),
+          lat: +map.current.getCenter().lat.toFixed(4),
+          zoom: +map.current.getZoom().toFixed(2),
+        });
+      });
+    }
+  }, []);
+
   return (
-    <h1>Map component</h1>
+    <div>
+      <MapContainer ref={mapContainer}/>
+    </div>
   );
 }
 
