d75acb9349bff67b8eded32d14ea1314b1fceb6c
ilyamore88 <themorozik@gmail.com>
2021-02-06 13:25:56 +0300

    move global styles, create file with color variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
 import React, { ReactElement } from 'react';
-import { createGlobalStyle } from 'styled-components';
 import HomePage from './pages/HomePage';
 import { Switch, Route } from 'react-router-dom';
-
-const GlobalStyle = createGlobalStyle`
-  html, body {
-    height: 100%;
-  }
-
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
-  }
-
-  *, *::after, *::before {
-    box-sizing: border-box;
-  }
-`;
+import GlobalStyles from './styles/GlobalStyles';
+import ColorVariables from './styles/ColorVariables';
 
 /**
  * Main component of the application
@@ -32,7 +10,8 @@ const GlobalStyle = createGlobalStyle`
 function App(): ReactElement {
   return (
     <>
-      <GlobalStyle/>
+      <ColorVariables/>
+      <GlobalStyles/>
       <Switch>
         <Route path="/">
           <HomePage/>
