82c74c1470bbccc0bfe1b2acbcda54d2c9c3ea9a
ilyamore88 <themorozik@gmail.com>
2021-02-06 13:51:06 +0300

    add left panel component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import { Switch, Route } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import ColorVariables from './styles/ColorVariables';
+import ShadowVariables from './styles/ShadowVariables';
 
 /**
  * Main component of the application
@@ -11,6 +12,7 @@ function App(): ReactElement {
   return (
     <>
       <ColorVariables/>
+      <ShadowVariables/>
       <GlobalStyles/>
       <Switch>
         <Route path="/">
---
+++ src/styles/ShadowVariables.ts
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from 'styled-components';
+
+/**
+ * CSS box shadow variables
+ */
+const ShadowVariables = createGlobalStyle`
+:root {
+  --shadow-base: 0 4px 8px rgba(0, 0, 0, 0.04), 0 0 2px rgba(0, 0, 0, 0.06), 0 0 1px rgba(0, 0, 0, 0.04);
+}
+`;
+
+export default ShadowVariables;
