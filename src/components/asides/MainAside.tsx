d826b121b109de96053f0e09e8556def75e6669a
ilyamore88 <themorozik@gmail.com>
2021-03-30 22:40:32 +0300

    add buttons to aside uses router

--- src/components/asides/MainAside.tsx
+++ src/components/asides/MainAside.tsx
@@ -9,6 +9,10 @@ import MenuAsideContext from '../../contexts/MenuAsideContext';
 import CustomSelect from '../CustomSelect';
 import CustomRange from '../CustomRange';
 import { useTranslation } from 'react-i18next';
+import AsideBottomButton from '../AsideBottomButton';
+import MapIcon from '../../assets/map.svg';
+import SearchIcon from '../../assets/search.svg';
+import { Route, Switch, useHistory } from 'react-router-dom';
 
 const AsideCloseButtonPositioned = styled(AsideCloseButton)`
   position: absolute;
@@ -24,6 +28,32 @@ const SearchLineWithMarginBottom = styled(SearchLine)`
   margin-bottom: 12px;
 `;
 
+const BottomButton = styled(AsideBottomButton)`
+  margin-top: auto;
+`;
+
+const BottomButtonIcon = styled.div`
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  margin-right: 8px;
+`;
+
+const MapBottomButtonIcon = styled(BottomButtonIcon)`
+  width: 26px;
+  height: 22px;
+
+  background-image: url("${MapIcon}");
+`;
+
+const SearchBottomButtonIcon = styled(BottomButtonIcon)`
+  width: 24px;
+  height: 24px;
+
+  background-image: url("${SearchIcon}");
+`;
+
 /**
  * Main aside component
  */
@@ -31,6 +61,7 @@ function MainAside(): ReactElement {
   const { t } = useTranslation();
   const [showAside, setShowAside] = useState(true);
   const [isMenuAsideShow, setMenuAsideShow] = useState(false);
+  const history = useHistory();
 
   return (
     <LeftPanel show={showAside}>
@@ -53,6 +84,21 @@ function MainAside(): ReactElement {
           label={t(`customRange.years`)}
         />
         Next elements for test
+
+        <Switch>
+          <Route exact path="/">
+            <BottomButton onClick={() => history.push('/routes')}>
+              <MapBottomButtonIcon/>
+              {t('aside.routesBottomButton')}
+            </BottomButton>
+          </Route>
+          <Route path="/routes">
+            <BottomButton onClick={() => history.push('/')}>
+              <SearchBottomButtonIcon/>
+              {t('aside.searchBottomButton')}
+            </BottomButton>
+          </Route>
+        </Switch>
       </MenuAsideContext.Provider>
     </LeftPanel>
   );
