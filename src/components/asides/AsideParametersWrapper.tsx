181273083af6a8b7e357d2659b1a545b3a784899
ilyamore88 <themorozik@gmail.com>
2021-03-31 13:09:30 +0300

    refactor ReactElement to styled div

--- src/components/asides/AsideParametersWrapper.tsx
+++ src/components/asides/AsideParametersWrapper.tsx
@@ -1,25 +1,11 @@
-import { ReactElement } from 'react';
-import WithChildren from '../../interfaces/WithChildren';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+/**
+ * Parameters wrapper for asides with box-shadow
+ */
+export default styled.div`
   padding: 0 16px 24px;
   margin: 0 -16px;
 
   box-shadow: var(--shadow-base);
 `;
-
-interface AsideParametersWrapperProps extends WithChildren {}
-
-/**
- * Wrapper for parameters in aside with box-shadow
- *
- * @param props - props of component
- */
-export default function AsideParametersWrapper(props: AsideParametersWrapperProps): ReactElement {
-  return (
-    <Wrapper>
-      { props.children }
-    </Wrapper>
-  );
-}
