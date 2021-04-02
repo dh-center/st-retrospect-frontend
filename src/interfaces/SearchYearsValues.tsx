c19bf254d380095750cf823865f264536e82a08d
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-04-02 13:25:58 +0300

    added interface for end values of search components

---
+++ src/interfaces/SearchYearsValues.tsx
@@ -0,0 +1,14 @@
+/**
+ * Interface of props for CustomSelect components
+ */
+export interface SearchYearsValues {
+  /**
+   * Left value of range
+   */
+  left: string;
+
+  /**
+   * Right value of range
+   */
+  right: string;
+}
