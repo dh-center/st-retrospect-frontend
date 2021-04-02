513237c73f379ed50c8ef0d38791aa70c5ae0431
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-04-02 13:25:40 +0300

    added interface for properties of search components

---
+++ src/interfaces/SearchProps.tsx
@@ -0,0 +1,38 @@
+import { SearchYearsValues } from './SearchYearsValues';
+
+/**
+ * Interface of props for CustomSelect components
+ */
+export interface SearchProps {
+  /**
+   * onChange event handler
+   *
+   * @param values - end values of years
+   */
+  onChange: (values: SearchYearsValues) => void;
+
+  /**
+   * Minimum year for searching
+   */
+  min: string;
+
+  /**
+   * Maximum year for searching
+   */
+  max: string;
+
+  /**
+   * Left value of range
+   */
+  left: string;
+
+  /**
+   * Right value of range
+   */
+  right: string;
+
+  /**
+   * Label for range input
+   */
+  label?: string;
+}
