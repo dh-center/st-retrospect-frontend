350926abd90e699db0cb1f8cfbac2fa04ccc6844
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-04-09 23:32:33 +0300

    added interface for CustomSelect.tsx

---
+++ src/interfaces/OnChangeSelected.tsx
@@ -0,0 +1,11 @@
+/**
+ * Interface for sending selected items of custom select
+ */
+export interface OnChangeSelected {
+  /**
+   * onChange event handler
+   *
+   * @param values - all selected values
+   */
+  onChange: (values: string[]) => void;
+}
