5646b675732608196160ae9658f74f4bad3a9d96
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-04-22 23:47:31 +0300

    connected Menu and MenuContent in MenuAside

--- src/components/asides/MenuAside.tsx
+++ src/components/asides/MenuAside.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useContext } from 'react';
+import { ReactElement, useContext, useState } from 'react';
 import LeftPanel from '../LeftPanel';
 import AsideHeader from './AsideHeader';
 import styled from 'styled-components';
@@ -6,6 +6,7 @@ import MenuAsideContext from '../../contexts/MenuAsideContext';
 import DoubleArrowsIcon from '../../assets/double-arrows.svg';
 import Menu from '../Menu';
 import Overlay from '../Overlay';
+import MenuContent from './../MenuContent';
 
 const LeftPanelWithLargeShadow = styled(LeftPanel)`
   box-shadow: var(--shadow-large);
@@ -34,6 +35,7 @@ const AsideHeaderWithMarginBottom = styled(AsideHeader)`
  */
 function MenuAside(): ReactElement {
   const { isMenuAsideShow, setMenuAsideShow } = useContext(MenuAsideContext);
+  const [selectedMenuItem, setSelectedMenuItem] = useState<number>(0);
 
   return (
     <>
@@ -42,8 +44,12 @@ function MenuAside(): ReactElement {
         <AsideHeaderWithMarginBottom>
           <CloseMenuButton onClick={() => setMenuAsideShow(!isMenuAsideShow)}/>
         </AsideHeaderWithMarginBottom>
-        <Menu/>
+        <Menu onChange={(item) => {
+          item === selectedMenuItem && setSelectedMenuItem(0);
+          ((item !== selectedMenuItem) || (selectedMenuItem === 0)) && setSelectedMenuItem(item);
+        }}/>
       </LeftPanelWithLargeShadow>
+      <MenuContent selectedMenuItem={selectedMenuItem}/>
     </>
   );
 }
