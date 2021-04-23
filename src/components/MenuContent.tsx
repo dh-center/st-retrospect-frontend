8da8c5ee3119696730be057d29d206ba9fbc57e1
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-04-24 01:04:50 +0300

    refactored MenuContent.tsx

--- src/components/MenuContent.tsx
+++ src/components/MenuContent.tsx
@@ -1,19 +1,21 @@
 import { ReactElement } from 'react';
 import styled from 'styled-components';
+import WithChildren from '../interfaces/WithChildren';
 
-interface MenuContentProps {
+interface MenuContentProps extends WithChildren {
   /**
    * Number of selected menu item (0 - never selected)
    */
-  selectedMenuItem: number;
+  isMenuContentShow: boolean;
 }
 
 const MenuContentWrapper = styled.div<MenuContentProps>`
-  background-color: red;
+  background-color: var(--color-white);
 
   display: inline-block;
 
   height: 100vh;
+  overflow: scroll;
   width: calc(100vw - 372px);
 
   padding: 0;
@@ -21,7 +23,7 @@ const MenuContentWrapper = styled.div<MenuContentProps>`
 
   position: absolute;
   top: 0;
-  right: ${props => (props.selectedMenuItem !== 0) ? 'calc(372px - 100vw)' : 'calc((372px - 100vw)*2)'};
+  right: ${props => props.isMenuContentShow ? 'calc(372px - 100vw)' : 'calc((372px - 100vw)*2)'};
   z-index: 3;
 
   transition: right ease-out .5s;
@@ -33,17 +35,11 @@ const MenuContentWrapper = styled.div<MenuContentProps>`
  * @param props - properties (menu selected items)
  */
 function MenuContent(props: MenuContentProps): ReactElement {
-
   return (
     <>
-      { (props.selectedMenuItem !== 0) &&
-        <MenuContentWrapper selectedMenuItem={props.selectedMenuItem}>
-          <>
-            { (props.selectedMenuItem === 1) && <>About Project</> }
-            { (props.selectedMenuItem === 2) && <>How to use map?</> }
-            { (props.selectedMenuItem === 3) && <>Our partners</> }
-            { (props.selectedMenuItem === 4) && <>Thanks</> }
-          </>
+      { props.isMenuContentShow &&
+        <MenuContentWrapper isMenuContentShow={props.isMenuContentShow}>
+          { props.children }
         </MenuContentWrapper>
       }
     </>
