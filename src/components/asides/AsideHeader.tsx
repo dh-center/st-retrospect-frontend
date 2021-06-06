f0b123f9f25fc2d594bed7bb657070eb8c9a6da1
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-06-06 12:03:16 +0300

    added search switch button to header (& blue icons)

--- src/components/asides/AsideHeader.tsx
+++ src/components/asides/AsideHeader.tsx
@@ -4,6 +4,10 @@ import ApplicationLogo from '../ApplicationLogo';
 import LanguageSwitch from '../LanguageSwitch';
 import WithClassName from '../../interfaces/WithClassName';
 import WithChildren from '../../interfaces/WithChildren';
+import { Route, Switch, useHistory } from 'react-router';
+import MapIconBlue from '../../assets/map-blue.svg';
+import SearchIconBlue from '../../assets/search-blue.svg';
+import useBreakpoint from '../../lib/useBreakpoint';
 
 /**
  * AsideHeader component props
@@ -27,6 +31,34 @@ const HeaderWrapper = styled.header`
 
 const ApplicationLogoWithFlex = styled(ApplicationLogo)`
   flex: 1;
+`;
+
+const ButtonIcon = styled.div`
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  box-sizing: border-box;
+
+  margin-right: 15px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const MapButtonIcon = styled(ButtonIcon)`
+  width: 26px;
+  height: 22px;
+
+  background-image: url("${MapIconBlue}");
+`;
+
+const SearchButtonIcon = styled(ButtonIcon)`
+  width: 24px;
+  height: 24px;
+
+  background-image: url("${SearchIconBlue}");
 `;
 
 /**
