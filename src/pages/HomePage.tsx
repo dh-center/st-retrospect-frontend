2a51745622ed203abe041be77f948d2e8e43bcbc
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-06-21 22:41:35 +0300

    added components for tablet view

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,26 @@
 import { ReactElement } from 'react';
 import MapView from '../components/MapView';
 import MainAside from '../components/asides/MainAside';
+import MainAsidePocket from '../components/asides/MainAsidePocket';
+import useBreakpoint from '../lib/useBreakpoint';
 
 /**
  * View of home page
  */
 function HomePage(): ReactElement {
+  const breakpoint = useBreakpoint();
+
   return (
-    <div>
-      <MainAside/>
-      <MapView/>
-    </div>
+    <>
+      { breakpoint.isPocket && <div>
+        <MainAsidePocket/>
+        <MapView/>
+      </div> }
+      { !breakpoint.isPocket && <div>
+        <MainAside/>
+        <MapView/>
+      </div> }
+    </>
   );
 }
 
