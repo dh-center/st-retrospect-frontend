de17ea9f0ea9b66878c899ab81d8b38d0a9f7d12
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-06-21 22:42:38 +0300

    removed unusual styles

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -20,11 +20,6 @@ const LeftPanelWrapper = styled.aside<LeftPanelProps>`
   background: var(--color-white);
   box-shadow: var(--shadow-base);
   transition: left ease-out .3s;
-
-  @media(max-width: 768px) {
-    width: 100vw;
-    left: ${props => props.show ? '0' : '-100vw'};
-  }
 `;
 
 /**
